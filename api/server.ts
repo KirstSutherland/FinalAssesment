c7c10b5035503827bca2d92cbafe95fc702c3485
Kirsten Sutherland <kirstensuther@gmail.com>
2024-01-12 16:19:18 +0200

    Creating a query to read all the subjects

--- api/server.ts
+++ api/server.ts
@@ -1,19 +1,31 @@
+
+const mongo = require('mongodb')
 const express = require('express');
-var cors = require('cors');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use(cors());
+// MongoDB connection URI
+const uri = "mongodb://localhost:27017/";
+// Create a new instance of MongoClient with the specified URI
+const client = new mongo.MongoClient(uri);
 
-app.get('/subjects', (req, res) => {
+app.get('/subjects', async (req, res) => {
     // get data from db
     // const result = await db.collection('subjects').find().toArray()
 
-    
+    //Query for a subject Maths
+
+    // Query for a subject with the given id ('659be0ba55536751c8c91d18')
+    const query = { subjectID: '1' };
+
+    // Use the MongoClient to connect to the 'EducationApp' database
+    // and retrieve the 'Subject' collection, then find one document that matches the query
+    const result = await client.db('EducationApp').collection('Subjects').find().toArray();
 
 
-    // res.send(result)
-    res.send([
+    res.send(result)
+    /**res.send([
         {
             name: 'Maths',
             id: 1
@@ -22,7 +34,7 @@ app.get('/subjects', (req, res) => {
             name: 'Science',
             id: 2
         }
-    ]);
+    ]);*/
 });
 
 app.listen(port, () => {
